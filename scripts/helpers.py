12ea77241f132f293b2205c60e6dd8bca1ecbbb4
Matthieu de Hemptinne <matthieu.dehemptinne@gmail.com>
2025-08-26 16:36:38 +0200

    done with creating synthetic genotypes

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,7 +1,7 @@
-def map_to_color(x, y, z, df):
+def map_to_color(x, y, z, df, value):
     # Check if there's variance to avoid division by zero
     r = x / df['x'].max() if df['x'].max() != 0 else 0  # Red component based on 'x'
     g = y / df['y'].max() if df['y'].max() != 0 else 0  # Green component based on 'y'
-    b = z / df['z'].max() if df['z'].max() != 0 else 0  # Blue component based on 'z'
+    b = z / df[value].max() if df[value].max() != 0 else 0  # Blue component based on 'z'
     
     return (r, g, b)
