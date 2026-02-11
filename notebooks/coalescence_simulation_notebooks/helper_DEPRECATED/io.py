521257be1d60ccc4571cbacdebbbcf41ba5846fe
Matthieu de Hemptinne <matthieu.dehemptinne@gmail.com>
2026-02-11 12:21:33 +0200

    starting from scratch

--- notebooks/coalescence_simulation_notebooks/helper_DEPRECATED/io.py
+++ notebooks/coalescence_simulation_notebooks/helper_DEPRECATED/io.py
@@ -20,5 +20,5 @@ def load_data():
     humans = pd.read_pickle(f"{path_pheno}/humans.pkl")
 
     path_geno = f"simulation_data/G{G}_L{L}_c{c}_k{k}_M{M}_F{F_outbred}/genotype/"
-    geno = pd.read_pickle(f"{path_geno}/complete.pkl")
+    geno = (pd.read_pickle(f"{path_geno}/complete.pkl") + 1) / 2  # stays between 0 and 1
     return geno, humans
